d9417f4d0c48d04298ab32290c3b1ffbd6aaf88b
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-06-14 19:19:28 -0300

    criando Footer

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+width: 100%;
+height: 40px;
+background: #20295f;
+border-top: 5px solid #EE6B26;
+position: fixed;
+bottom: 0;
+
+display: flex;
+justify-content: center;
+align-items: center;
+
+span{
+    color: #fff;
+    
+}
+`
