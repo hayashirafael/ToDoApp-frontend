896f1e8d9a9cda9576e9667ca53b17f7aef4945b
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-06-14 18:43:54 -0300

    criando header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import * as S from './styles'
+
+import logo from '../../assets/logo.png'
+import bell from '../../assets/bell.png'
 
 function Header(){
-    return <h1>Header AQUI</h1>
+    return (
+        <S.Container>
+            <S.LeftSide>
+                <img src={logo} alt="Logo"/>
+            </S.LeftSide>
+            <S.RightSide>
+                <a href="#">INÍCIO</a>
+                <span className="divisor"/>
+                <a href="#">NOVA TAREFA</a>
+                <span className="divisor"/>
+                <a href="#">SINCRONIZAR CELULAR</a>
+                <span className="divisor"/>
+                <a href="#" id="notification">
+                    <img src={bell} alt="Notificação"/>
+                    <span>5</span>
+                </a>
+            </S.RightSide>
+        </S.Container>
+    )
 }
 
 export default Header;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from '../../components/Header'
+import * as S from './styles'
 
 
 function Home() {
-    return <Header/>
+    return (
+        <S.Container>
+            <Header/>
+        </S.Container>
+
+    )
 }
 
 
